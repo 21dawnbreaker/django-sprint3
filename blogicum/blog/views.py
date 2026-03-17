08c1333a10ef872b047e63d08ca1166fd8d06607
21dawnbreaker <kpotapov0912@gmail.com>
2026-03-17 05:44:56 +0300

    upd blog/views

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,8 +1,8 @@
 import datetime
 
-from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from blog.models import Post, Category, Location
+from tests.fixtures.fixture_data import published_category
 
 
 def index(request):
@@ -18,25 +18,34 @@ def index(request):
     return render(request, 'blog/index.html', context)
 
 
-
 def post_detail(request, post_id: int):
-    if post_id not in post_per_key:
-        raise Http404('Post not found')
+    post = get_object_or_404(
+        Post.objects.select_related('category', 'location'),
+        id=post_id,
+        pub_date__lte=datetime.datetime.now(),
+        is_published=True,
+        category__is_published=True,
+    )
+
     context = {
-        'post': post_per_key[post_id],
+        'post': post,
     }
     return render(request, 'blog/detail.html', context)
 
 
 def category_posts(request, category_slug):
-    published_category = get_object_or_404(Category, slug=category_slug).is_published
-    context = {
-        'category_slug': Post.objects.select_related(
+    category = get_object_or_404(Category, slug=category_slug)
+    post_list = Post.objects.select_related(
             'category', 'location',
         ).filter(
-            category__is_published=published_category,
+            category__is_published=category.is_published,
             category__slug=category_slug,
             pub_date__lte=datetime.datetime.now(),
-        ),
+        )
+
+    context = {
+        'post_list': post_list,
+        'category': category_slug,
     }
+    print(context)
     return render(request, 'blog/category.html', context)
