4085d40eab2e3d3e09ea317aa3b102eccffaa5e6
21dawnbreaker <kpotapov0912@gmail.com>
2026-03-18 00:09:20 +0300

    fixes after code review

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,19 +1,65 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 from core.models import PublishedModel
 
-from django.contrib.auth import get_user_model
-
 User = get_user_model()
+
+CHAR_FIELD_MAX_LENGTH = 256
+
+
+class Category(PublishedModel):
+    title = models.CharField(
+        max_length=CHAR_FIELD_MAX_LENGTH,
+        verbose_name='Заголовок'
+    )
+    description = models.TextField(verbose_name='Описание')
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Идентификатор',
+        help_text=(
+            'Идентификатор страницы для URL; '
+            'разрешены символы латиницы, цифры, '
+            'дефис и подчёркивание.'
+        )
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+        default_related_name = 'categories'
+
+
+class Location(PublishedModel):
+    name = models.CharField(
+        max_length=CHAR_FIELD_MAX_LENGTH,
+        verbose_name='Название места'
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
+        default_related_name = 'locations'
 
 
 class Post(PublishedModel):
-    title = models.CharField(max_length=256, verbose_name='Заголовок')
+    title = models.CharField(
+        max_length=CHAR_FIELD_MAX_LENGTH,
+        verbose_name='Заголовок'
+    )
     text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
-        help_text='Если установить дату и время в будущем — '
-                  'можно делать отложенные публикации.'
+        help_text=(
+            'Если установить дату и время в будущем — '
+            'можно делать отложенные публикации.'
+        )
     )
     author = models.ForeignKey(
         User,
@@ -21,43 +67,24 @@ class Post(PublishedModel):
         verbose_name='Автор публикации'
     )
     location = models.ForeignKey(
-        'Location',
+        Location,
         blank=True,
         on_delete=models.SET_NULL,
         null=True,
         verbose_name='Местоположение',
     )
     category = models.ForeignKey(
-        'Category',
+        Category,
         on_delete=models.SET_NULL,
         null=True,
         verbose_name='Категория',
     )
 
+    def __str__(self):
+        return self.title
+
     class Meta:
         verbose_name = 'публикация'
         verbose_name_plural = 'Публикации'
-
-
-class Category(PublishedModel):
-    title = models.CharField(max_length=256, verbose_name='Заголовок')
-    description = models.TextField(verbose_name='Описание')
-    slug = models.SlugField(
-        unique=True,
-        verbose_name='Идентификатор',
-        help_text='Идентификатор страницы для URL; '
-                  'разрешены символы латиницы, цифры, '
-                  'дефис и подчёркивание.'
-    )
-
-    class Meta:
-        verbose_name = 'категория'
-        verbose_name_plural = 'Категории'
-
-
-class Location(PublishedModel):
-    name = models.CharField(max_length=256, verbose_name='Название места')
-
-    class Meta:
-        verbose_name = 'местоположение'
-        verbose_name_plural = 'Местоположения'
+        ordering = ('-pub_date', 'id')
+        default_related_name = 'posts'
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,29 +1,38 @@
-import datetime
-
 from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+
 from blog.models import Post, Category
+
+DISPLAYED_POSTS_AMOUNT = 5
+
+
+def get_filtered_query_set():
+    """Вспомогательная функция, возвращает
+    выборку с базовыми фильтрами.
+    """
+    query_set = Post.objects.select_related(
+        'category', 'location', 'author'
+    ).filter(
+        category__is_published=True,
+        is_published=True,
+        pub_date__lte=timezone.now(),
+    )
+    return query_set
 
 
 def index(request):
+    posts_list = get_filtered_query_set()
+
     context = {
-        'posts': Post.objects.select_related(
-            'category', 'location',
-        ).filter(
-            pub_date__lte=datetime.datetime.now(),
-            is_published=True,
-            category__is_published=True,
-        ).order_by('-pub_date')[:5],
+        'posts': posts_list[:DISPLAYED_POSTS_AMOUNT],
     }
     return render(request, 'blog/index.html', context)
 
 
 def post_detail(request, post_id: int):
     post = get_object_or_404(
-        Post.objects.select_related('category', 'location'),
+        get_filtered_query_set(),
         id=post_id,
-        pub_date__lte=datetime.datetime.now(),
-        is_published=True,
-        category__is_published=True,
     )
 
     context = {
@@ -33,21 +42,17 @@ def post_detail(request, post_id: int):
 
 
 def category_posts(request, category_slug):
-    category = get_object_or_404(
-        Category, slug=category_slug, is_published=True
+    get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True
     )
-    post_list = Post.objects.select_related(
-        'category', 'location'
-    ).filter(
-        category__is_published=category.is_published,
+    post_list = get_filtered_query_set().filter(
         category__slug=category_slug,
-        is_published=True,
-        pub_date__lte=datetime.datetime.now()
     )
 
     context = {
         'post_list': post_list,
         'category': category_slug,
     }
-    print(context)
     return render(request, 'blog/category.html', context)
